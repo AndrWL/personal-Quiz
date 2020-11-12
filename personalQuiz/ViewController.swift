323b4ca99a5edc77338dd8f6cd530bb008e1c130
AndrWL <andrewlebedev13@gmail.com>
2020-11-12 19:45:23 +0200

    Initial Commit

---
+++ personalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  personalQuiz
+//
+//  Created by Andrey on 12.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
